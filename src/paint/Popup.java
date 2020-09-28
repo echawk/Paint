d94ee7e73ba625d2c44dd7a362f72b817864dc56
Ethan <ethan.hawk@valpo.edu>
2020-09-27 19:19:18 -0500

    Added a showImage to Popup.java, this method is meant to help with debuging

--- src/paint/Popup.java
+++ src/paint/Popup.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
 import javafx.scene.layout.GridPane;
@@ -148,5 +149,15 @@ public class Popup {
 		closeConfirmStage.show();
 	}
 	
+	public static void showImage(Image i) {
+		Stage showImage = new Stage();
+		showImage.initOwner(Paint.window);
+		ImageView iv = new ImageView(i);
+		VBox vb = new VBox();
+		vb.getChildren().addAll(iv);
+		Scene showImageScene = new Scene(vb, i.getWidth(), i.getHeight());
+		showImage.setScene(showImageScene);
+		showImage.show();
+	}
 	
 }
