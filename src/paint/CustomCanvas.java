bd6677bc46cbc80916e847f922db69b1cad5b7c8
Ethan <ethan.hawk@valpo.edu>
2020-09-28 11:02:41 -0500

    Changed the strings for the tools to be static class variables, so I now have a switch statement in CustomCanvas for selecting tools on a mouse press

--- src/paint/CustomCanvas.java
+++ src/paint/CustomCanvas.java
@@ -64,280 +64,239 @@ public class CustomCanvas extends Canvas{
 			this.gc.setLineWidth(Paint.brushSize);
 
 			if (Paint.getMode() == Paint.EDIT_MODE) {
-				if (Paint.edittoolbar.getDrawSelection().equals(
-						Paint.edittoolbar.LINE)) {
-					
-					this.gc.strokeLine(
-						this.mouseCoord.getKey(), 
-						this.mouseCoord.getValue(),
-						e.getX(),
-						e.getY()
-					);
-					Paint.getCurrentTab().imgHasBeenSaved = false;
-					
-				} else if (Paint.edittoolbar.getDrawSelection().equals(
-					Paint.edittoolbar.CIRCLE)) {
-					
-					double l;
-					//Use the larger dimension for drawing the circle
-					if (e.getX() >= e.getY()) {
-						l = (e.getX() - this.mouseCoord.getKey());
-					} else {
-						l = (e.getY() - this.mouseCoord.getValue());
-					}
-					this.gc.fillOval(
-						this.mouseCoord.getKey(), 
-						this.mouseCoord.getValue(),
-						l, 
-						l
-					);
-					Paint.getCurrentTab().imgHasBeenSaved = false;
-
-					
-				} else if (Paint.edittoolbar.getDrawSelection().equals(
-					Paint.edittoolbar.ELLIPSE)) {
-					
-					this.gc.fillOval(
-						this.mouseCoord.getKey(),
-						this.mouseCoord.getValue(),
-						(e.getX() - this.mouseCoord.getKey()),
-						(e.getY() - this.mouseCoord.getValue())
-					);
-					Paint.getCurrentTab().imgHasBeenSaved = false;
-
-					
-				} else if (Paint.edittoolbar.getDrawSelection().equals(
-					Paint.edittoolbar.RECTANGLE)) {
-					
-					this.gc.fillRect(
-						this.mouseCoord.getKey(), 
-						this.mouseCoord.getValue(), 
-						(e.getX() - this.mouseCoord.getKey()), 
-						(e.getY() - this.mouseCoord.getValue())
-					);
-					Paint.getCurrentTab().imgHasBeenSaved = false;
-
-					
-				} else if (Paint.edittoolbar.getDrawSelection().equals(
-					Paint.edittoolbar.SQUARE)) {
-					
-					double s;
-					//Use the larger dimension for drawing the square
-					if (e.getX() >= e.getY()) {
-						s = (e.getX() - this.mouseCoord.getKey());
-					} else {
-						s = (e.getY() - this.mouseCoord.getValue());
-					}
-					this.gc.fillRect(
-						this.mouseCoord.getKey(),
-						this.mouseCoord.getValue(),
-						s,
-						s
-					);
-					Paint.getCurrentTab().imgHasBeenSaved = false;
-
-					
-				} else if (Paint.edittoolbar.getDrawSelection().equals(
-					Paint.edittoolbar.TEXTBOX)) {
-					
-					this.gc.setFont(new Font(Paint.brushSize));
-					
-					this.gc.fillText(Paint.edittoolbar.getOptionsField(),
-						this.mouseCoord.getKey(),
-						this.mouseCoord.getValue()
-					);
-					Paint.getCurrentTab().imgHasBeenSaved = false;
-
-					
-				} else if (Paint.edittoolbar.getDrawSelection().equals(
-					Paint.edittoolbar.TRIANGLE)){
-					
-					Pair PolygonPts = getPolygonPoints(
-						3, 
-						this.mouseCoord, 
-						roundDouble(e.getX())
-					);
-					
-					double[] xp = (double[]) PolygonPts.getKey();
-					double[] yp = (double[]) PolygonPts.getValue();
-					
-					this.gc.fillPolygon(xp, yp, 3);
-					Paint.getCurrentTab().imgHasBeenSaved = false;
-
-					
-				} else if (Paint.edittoolbar.getDrawSelection().equals(
-					Paint.edittoolbar.NGON)) {
-					
-					int n = 0;
-					
-					try {
-						n = Integer.parseInt(Paint.edittoolbar.getOptionsField());
-					} catch (Exception ex) {
-						System.out.println("Failed to parse options field: " + ex);
-						return; // to keep from drawing a shape
-					}
-					Pair PolygonPts = getPolygonPoints(
-						n,
-						this.mouseCoord,
-						roundDouble(e.getX())
-					);
-					
-					double[] xp = (double[]) PolygonPts.getKey();
-					double[] yp = (double[]) PolygonPts.getValue();
-					
-					this.gc.fillPolygon(xp, yp, n);
-					Paint.getCurrentTab().imgHasBeenSaved = false;
-					
-
-				} else if (Paint.edittoolbar.getDrawSelection().equals(
-					Paint.edittoolbar.CROP)) {
-					
-					//I could use somethign similar to what I've done
-					//here for the drag and drop method
-					PixelReader r = this.getImage().getPixelReader();
-					WritableImage wi = new WritableImage(
-						r,
-						roundDouble(this.mouseCoord.getKey()),
-						roundDouble(this.mouseCoord.getValue()),
-						roundDouble(e.getX() - this.mouseCoord.getKey()),
-						roundDouble(e.getY() - this.mouseCoord.getValue())
-					);
-					Paint.getCurrentTab().setImage(wi);
-					Paint.getCurrentTab().imgHasBeenSaved = false;
-
-					
-				} else if (Paint.edittoolbar.getDrawSelection().equals(
-						Paint.edittoolbar.DRAGDROP)) {
-					
-					if (this.drag_drop_image == null) {
-						//Three steps:
-						//1 - get the image
-						//2 - make the image globally accessible
-						//3 - clear out a rectangle of the same size
-						
-						//1
-						PixelReader r = this.getImage().getPixelReader();
-						WritableImage wi = new WritableImage(
-							r,
-							roundDouble(this.mouseCoord.getKey()),
-							roundDouble(this.mouseCoord.getValue()),
-							roundDouble(e.getX() - this.mouseCoord.getKey()),
-							roundDouble(e.getY() - this.mouseCoord.getValue())
-						);
-						//2
-						this.drag_drop_image = wi;
-						//3
-						this.gc.clearRect(
-							roundDouble(this.mouseCoord.getKey()),
-							roundDouble(this.mouseCoord.getValue()),
-							roundDouble(e.getX() - this.mouseCoord.getKey()),
-							roundDouble(e.getY() - this.mouseCoord.getValue())
-						);
-						//Exit
-						return;
-					}
-					
-					this.gc.drawImage(
-						this.drag_drop_image, 
-						e.getX(), 
-						e.getY()
-					);
-					//set the image back to null
-					this.drag_drop_image = null;
-					Paint.getCurrentTab().imgHasBeenSaved = false;
-
-					
-				} else if (Paint.edittoolbar.getDrawSelection().equals(
-						Paint.edittoolbar.BLUR)) {
-					//INCOMPLETE
-					
-					//Three steps:
-					//1 get image
-					//2 apply blur effect to image
-					//3 draw the new image
-					
-					//1
-					PixelReader r = this.getImage().getPixelReader();
-					WritableImage wi = new WritableImage(
-						r,
-						roundDouble(this.mouseCoord.getKey()),
-						roundDouble(this.mouseCoord.getValue()),
-						roundDouble(e.getX() - this.mouseCoord.getKey()),
-						roundDouble(e.getY() - this.mouseCoord.getValue())
-					);
-					
-					CustomCanvas t = new CustomCanvas();
-					t.updateDimensions(wi); //need to make sure the canvas has dimensions
-					t.gc.setEffect(new GaussianBlur());
-					t.gc.drawImage(wi, 0, 0);
-					//Popup.showImage(t.getImage()); //DEBUG
-					this.gc.drawImage(
-						t.getImage(),
-						this.mouseCoord.getKey(),
-						this.mouseCoord.getValue()
-					);			
-					Paint.getCurrentTab().imgHasBeenSaved = false;
-
-				}  else if (Paint.edittoolbar.getDrawSelection().equals(
-						Paint.edittoolbar.SEPIA)) {
-					PixelReader r = this.getImage().getPixelReader();
-					WritableImage wi = new WritableImage(
-						r,
-						roundDouble(this.mouseCoord.getKey()),
-						roundDouble(this.mouseCoord.getValue()),
-						roundDouble(e.getX() - this.mouseCoord.getKey()),
-						roundDouble(e.getY() - this.mouseCoord.getValue())
-					);
-					
-					CustomCanvas t = new CustomCanvas();
-					t.updateDimensions(wi); //need to make sure the canvas has dimensions
-					t.gc.setEffect(new SepiaTone());
-					t.gc.drawImage(wi, 0, 0);
-					//Popup.showImage(t.getImage()); //DEBUG
-					this.gc.drawImage(
-						t.getImage(),
-						this.mouseCoord.getKey(),
-						this.mouseCoord.getValue()
-					);			
-					Paint.getCurrentTab().imgHasBeenSaved = false;
-
-				
-				
-				} else if (Paint.edittoolbar.getDrawSelection().equals(
-						Paint.edittoolbar.ROTATE)) {
-					
-					//INCOMPLETE
-					//Three steps:
-					//1 - get selection
-					//2 - rotate selectin
-					//3 - draw rotated selection
-					
-					//1
-					PixelReader r = this.getImage().getPixelReader();
-					WritableImage wi = new WritableImage(
-						r,
-						roundDouble(this.mouseCoord.getKey()),
-						roundDouble(this.mouseCoord.getValue()),
-						roundDouble(e.getX() - this.mouseCoord.getKey()),
-						roundDouble(e.getY() - this.mouseCoord.getValue())
-					);
-					
-					//2
-					CustomCanvas t = new CustomCanvas();
-					t.updateDimensions(wi);
-					t.gc.save();
-					t.gc.rotate(Double.parseDouble(Paint.edittoolbar.getOptionsField()));
-					t.gc.drawImage(wi, 0, 0);
-					t.gc.restore();
-					//Popup.showImage(t.getImage()); //DEBUG
-					//3
-					this.gc.drawImage(
-						t.getImage(),
-						this.mouseCoord.getKey(),
-						this.mouseCoord.getValue()
-					);
-					
-					Paint.getCurrentTab().imgHasBeenSaved = false;
+				switch (Paint.edittoolbar.getDrawSelection()) {
+					case EditToolBar.LINE:
+						this.gc.strokeLine(
+							this.mouseCoord.getKey(),
+							this.mouseCoord.getValue(),
+							e.getX(),
+							e.getY()
+						);	Paint.getCurrentTab().imgHasBeenSaved = false;
+						break;
+					case EditToolBar.CIRCLE:
+						double l;
+						//Use the larger dimension for drawing the circle
+						if (e.getX() >= e.getY()) {
+							l = (e.getX() - this.mouseCoord.getKey());
+						} else {
+							l = (e.getY() - this.mouseCoord.getValue());
+						}	
+						this.gc.fillOval(
+							this.mouseCoord.getKey(),
+							this.mouseCoord.getValue(),
+							l,
+							l
+						);	Paint.getCurrentTab().imgHasBeenSaved = false;
+						break;
+					case EditToolBar.ELLIPSE:
+						this.gc.fillOval(
+							this.mouseCoord.getKey(),
+							this.mouseCoord.getValue(),
+							(e.getX() - this.mouseCoord.getKey()),
+							(e.getY() - this.mouseCoord.getValue())
+						);	Paint.getCurrentTab().imgHasBeenSaved = false;
+						break;
+					case EditToolBar.RECTANGLE:
+						this.gc.fillRect(
+							this.mouseCoord.getKey(),
+							this.mouseCoord.getValue(),
+							(e.getX() - this.mouseCoord.getKey()),
+							(e.getY() - this.mouseCoord.getValue())
+						);	Paint.getCurrentTab().imgHasBeenSaved = false;
+						break;
+					case EditToolBar.SQUARE:
+						double s;
+						//Use the larger dimension for drawing the square
+						if (e.getX() >= e.getY()) {
+							s = (e.getX() - this.mouseCoord.getKey());
+						} else {
+							s = (e.getY() - this.mouseCoord.getValue());
+						}	
+						this.gc.fillRect(
+							this.mouseCoord.getKey(),
+							this.mouseCoord.getValue(),
+							s,
+							s
+						);	Paint.getCurrentTab().imgHasBeenSaved = false;
+						break;
+					case EditToolBar.TEXTBOX:
+						this.gc.setFont(new Font(Paint.brushSize));
+						this.gc.fillText(Paint.edittoolbar.getOptionsField(),
+							this.mouseCoord.getKey(),
+							this.mouseCoord.getValue()
+						);	
+						Paint.getCurrentTab().imgHasBeenSaved = false;
+						break;
+					case EditToolBar.TRIANGLE:
+						{
+							Pair PolygonPts = getPolygonPoints(
+								3,
+								this.mouseCoord,
+								roundDouble(e.getX())
+							);		double[] xp = (double[]) PolygonPts.getKey();
+							double[] yp = (double[]) PolygonPts.getValue();
+							this.gc.fillPolygon(xp, yp, 3);
+							Paint.getCurrentTab().imgHasBeenSaved = false;
+							break;
+						}
+					case EditToolBar.NGON:
+						{
+							int n = 0;
+							try {
+								n = Integer.parseInt(Paint.edittoolbar.getOptionsField());
+							} catch (Exception ex) {
+								System.out.println("Failed to parse options field: " + ex);
+								return; // to keep from drawing a shape
+							}		Pair PolygonPts = getPolygonPoints(
+								n,
+								this.mouseCoord,
+								roundDouble(e.getX())
+							);		
+							double[] xp = (double[]) PolygonPts.getKey();
+							double[] yp = (double[]) PolygonPts.getValue();
+							this.gc.fillPolygon(xp, yp, n);
+							Paint.getCurrentTab().imgHasBeenSaved = false;
+							break;
+						}
+					case EditToolBar.CROP:
+						{
+							//I could use somethign similar to what I've done
+							//here for the drag and drop method
+							PixelReader r = this.getImage().getPixelReader();
+							WritableImage wi = new WritableImage(
+								r,
+								roundDouble(this.mouseCoord.getKey()),
+								roundDouble(this.mouseCoord.getValue()),
+								roundDouble(e.getX() - this.mouseCoord.getKey()),
+								roundDouble(e.getY() - this.mouseCoord.getValue())
+							);		
+							Paint.getCurrentTab().setImage(wi);
+							Paint.getCurrentTab().imgHasBeenSaved = false;
+							break;
+						}
+					case EditToolBar.DRAGDROP:
+						if (this.drag_drop_image == null) {
+							//Three steps:
+							//1 - get the image
+							//2 - make the image globally accessible
+							//3 - clear out a rectangle of the same size
+							
+							//1
+							PixelReader r = this.getImage().getPixelReader();
+							WritableImage wi = new WritableImage(
+								r,
+								roundDouble(this.mouseCoord.getKey()),
+								roundDouble(this.mouseCoord.getValue()),
+								roundDouble(e.getX() - this.mouseCoord.getKey()),
+								roundDouble(e.getY() - this.mouseCoord.getValue())
+							);
+							//2
+							this.drag_drop_image = wi;
+							//3
+							this.gc.clearRect(
+								roundDouble(this.mouseCoord.getKey()),
+								roundDouble(this.mouseCoord.getValue()),
+								roundDouble(e.getX() - this.mouseCoord.getKey()),
+								roundDouble(e.getY() - this.mouseCoord.getValue())
+							);
+							//Exit
+							return;
+						}	
+						this.gc.drawImage(
+							this.drag_drop_image,
+							e.getX(),
+							e.getY()
+						);	//set the image back to null
+						this.drag_drop_image = null;
+						Paint.getCurrentTab().imgHasBeenSaved = false;
+						break;
+					case EditToolBar.BLUR:
+						{
+							//INCOMPLETE
+							
+							//Three steps:
+							//1 get image
+							//2 apply blur effect to image
+							//3 draw the new image
+							
+							//1
+							PixelReader r = this.getImage().getPixelReader();
+							WritableImage wi = new WritableImage(
+								r,
+								roundDouble(this.mouseCoord.getKey()),
+								roundDouble(this.mouseCoord.getValue()),
+								roundDouble(e.getX() - this.mouseCoord.getKey()),
+								roundDouble(e.getY() - this.mouseCoord.getValue())
+							);		CustomCanvas t = new CustomCanvas();
+							t.updateDimensions(wi); //need to make sure the canvas has dimensions
+							t.gc.setEffect(new GaussianBlur());
+							t.gc.drawImage(wi, 0, 0);
+							//Popup.showImage(t.getImage()); //DEBUG
+							this.gc.drawImage(
+								t.getImage(),
+								this.mouseCoord.getKey(),
+								this.mouseCoord.getValue()
+							);		
+							Paint.getCurrentTab().imgHasBeenSaved = false;
+							break;
+						}
+					case EditToolBar.SEPIA:
+						{
+							PixelReader r = this.getImage().getPixelReader();
+							WritableImage wi = new WritableImage(
+								r,
+								roundDouble(this.mouseCoord.getKey()),
+								roundDouble(this.mouseCoord.getValue()),
+								roundDouble(e.getX() - this.mouseCoord.getKey()),
+								roundDouble(e.getY() - this.mouseCoord.getValue())
+							);		CustomCanvas t = new CustomCanvas();
+							t.updateDimensions(wi); //need to make sure the canvas has dimensions
+							t.gc.setEffect(new SepiaTone());
+							t.gc.drawImage(wi, 0, 0);
+							//Popup.showImage(t.getImage()); //DEBUG
+							this.gc.drawImage(
+								t.getImage(),
+								this.mouseCoord.getKey(),
+								this.mouseCoord.getValue()
+							);		
+							Paint.getCurrentTab().imgHasBeenSaved = false;
+							break;
+						}
+					case EditToolBar.ROTATE:
+						{
+							//INCOMPLETE
+							//Three steps:
+							//1 - get selection
+							//2 - rotate selectin
+							//3 - draw rotated selection
+							
+							//1
+							PixelReader r = this.getImage().getPixelReader();
+							WritableImage wi = new WritableImage(
+								r,
+								roundDouble(this.mouseCoord.getKey()),
+								roundDouble(this.mouseCoord.getValue()),
+								roundDouble(e.getX() - this.mouseCoord.getKey()),
+								roundDouble(e.getY() - this.mouseCoord.getValue())
+							);		//2
+							CustomCanvas t = new CustomCanvas();
+							t.updateDimensions(wi);
+							t.gc.save();
+							t.gc.rotate(Double.parseDouble(Paint.edittoolbar.getOptionsField()));
+							t.gc.drawImage(wi, 0, 0);
+							t.gc.restore();
+							//Popup.showImage(t.getImage()); //DEBUG
+							//3
+							this.gc.drawImage(
+								t.getImage(),
+								this.mouseCoord.getKey(),
+								this.mouseCoord.getValue()
+							);		
+							Paint.getCurrentTab().imgHasBeenSaved = false;
+							break;
+						}
+					default:
+						break;
 				}	
 			this.imgToStack(this.getImage());
 			}
--- src/paint/EditToolBar.java
+++ src/paint/EditToolBar.java
@@ -19,43 +19,43 @@ import javafx.scene.paint.Color;
  * @author ethan
  */
 public class EditToolBar extends ToolBar{
-	
+
 	private ComboBox drawoptionmenu;
 	private TextField brushfld;
 	private TextField options_fld;
 
-	public final String LINE = "Line";
-	public final String PENCIL = "Pencil";
-	public final String SQUARE = "Square";
-	public final String RECTANGLE = "Rectangle";
-	public final String CIRCLE = "Circle";
-	public final String ELLIPSE = "Ellipse";
-	public final String ERASE = "Erase";
-	public final String COLOR_GRAB = "Color Grab";
-	public final String NONE = "None";
-	public final String TEXTBOX = "Text Box";
-	public final String TRIANGLE = "Triangle";
-	public final String NGON = "N-gon";
-	public final String DRAGDROP = "Drag & Drop";
+	public static final String LINE = "Line";
+	public static final String PENCIL = "Pencil";
+	public static final String SQUARE = "Square";
+	public static final String RECTANGLE = "Rectangle";
+	public static final String CIRCLE = "Circle";
+	public static final String ELLIPSE = "Ellipse";
+	public static final String ERASE = "Erase";
+	public static final String COLOR_GRAB = "Color Grab";
+	public static final String NONE = "None";
+	public static final String TEXTBOX = "Text Box";
+	public static final String TRIANGLE = "Triangle";
+	public static final String NGON = "N-gon";
+	public static final String DRAGDROP = "Drag & Drop";
 	//Extras
-	public final String BLUR = "Blur";
-	public final String CROP = "Crop";
-	public final String ROTATE = "Rotate";
-	public final String SEPIA = "Sepia";
-	
+	public static final String BLUR = "Blur";
+	public static final String CROP = "Crop";
+	public static final String ROTATE = "Rotate"; //kinda broken
+	public static final String SEPIA = "Sepia";
+
 	public EditToolBar() {
-		
-		super();		
-		
+
+		super();
+
 		Button undobtn = new Button("Undo");
 			undobtn.setOnAction((ActionEvent event)-> {
 				try {
-					Paint.getCurrentTab().imgcanvas.undo();	
+					Paint.getCurrentTab().imgcanvas.undo();
 				} catch (Exception e) {
 					System.out.println("Undo Button On-Action Failed:" + e);
 				}
 			});
-		
+
 		Button redobtn = new Button("Redo");
 			redobtn.setOnAction((ActionEvent event) -> {
 				try {
@@ -64,40 +64,40 @@ public class EditToolBar extends ToolBar{
 					System.out.println("Redo Button On-Action Failed:" + e);
 				}
 			});
-			
+
 		Label drawlbl = new Label("Draw:");
-		
+
 		this.drawoptionmenu = new ComboBox();
 		//drawoptionmenu.autosize();
 		drawoptionmenu.getItems().addAll(
-				this.NONE,
-				this.LINE, 
-				this.PENCIL, 
-				this.SQUARE,
-				this.RECTANGLE, 
-				this.CIRCLE, 
-				this.ELLIPSE,
-				this.TRIANGLE,
-				this.NGON,
-				this.COLOR_GRAB,
-				this.TEXTBOX,
-				this.CROP,
-				this.DRAGDROP,
-				this.ROTATE, // broken
-				this.BLUR,
-				this.ERASE
+				EditToolBar.NONE,
+				EditToolBar.LINE,
+				EditToolBar.PENCIL,
+				EditToolBar.SQUARE,
+				EditToolBar.RECTANGLE,
+				EditToolBar.CIRCLE,
+				EditToolBar.ELLIPSE,
+				EditToolBar.TRIANGLE,
+				EditToolBar.NGON,
+				EditToolBar.COLOR_GRAB,
+				EditToolBar.TEXTBOX,
+				EditToolBar.CROP,
+				EditToolBar.DRAGDROP,
+				EditToolBar.ROTATE, // broken
+				EditToolBar.BLUR,
+				EditToolBar.ERASE
 		);
 		drawoptionmenu.setValue(this.NONE); //Set our default value to be NONE
-		
-		
+
+
 		Label optionslbl = new Label("Option:");
 		options_fld = new TextField();
 		options_fld.setMaxWidth(60); //Set to 60 to try to minimize the amount of wasted space.
-		
+
 		Label brushlbl = new Label("Brush (px):");
 
 		brushfld = new TextField(Double.toString(Paint.brushSize));
-		
+
 		brushfld.setMaxWidth(60); //change the Max width to something smaller (helps fit more on the first line)
 		//set the action for brushsize
 		brushfld.setOnAction((ActionEvent event) -> {
@@ -107,9 +107,9 @@ public class EditToolBar extends ToolBar{
 				System.out.println("Brush Field On-Action Failed:" + e);
 			}
 		});
-		
+
 		Label colorlbl = new Label("Color:");
-		
+
 		ColorPicker colorpick = Paint.colorpick;
 		Button resetbtn = new Button();
 		resetbtn.setText("Reset");
@@ -119,24 +119,24 @@ public class EditToolBar extends ToolBar{
 		});
 
 		this.getItems().addAll(
-			undobtn, 
+			undobtn,
 			redobtn,
-			drawlbl, 
-			drawoptionmenu, 
-			brushlbl, 
-			brushfld, 
-			colorlbl, 
-			colorpick, 
+			drawlbl,
+			drawoptionmenu,
+			brushlbl,
+			brushfld,
+			colorlbl,
+			colorpick,
 			optionslbl,
 			options_fld,
 			resetbtn
 		);
-	}	
-	
+	}
+
 	/**
-	 * This method is gets the string value of the selected option; this method is 
+	 * This method is gets the string value of the selected option; this method is
 	 * used primarily in the CustomCanvas Handling code.
-	 * 
+	 *
 	 * @return the String of the combobox's selected option
 	 */
 	public String getDrawSelection(){
@@ -144,13 +144,13 @@ public class EditToolBar extends ToolBar{
 	}
 	/**
 	 * Returns whatever string is present in options_fld.
-	 * 
+	 *
 	 * @return The String of the options field.
 	 */
 	public String getOptionsField(){
 		return this.options_fld.getText();
 	}
-	
+
 	/**
 	 * This method sets all of the configurable part of the edit tool bar back to their default values.
 	 */
